25c80a38e3ff41ca41544e1c854f4ba5534725a5
Dmitrii An <an.dmitriy93@gmail.com>
2021-08-28 19:22:42 -0500

    Finish with department details page. Overall project is fun, but so messy in code...

--- src/components/departmentDetails.jsx
+++ src/components/departmentDetails.jsx
@@ -1,9 +1,12 @@
-import { departments} from '../mocks';
+import { departments } from '../mocks';
 import { makeStyles } from '@material-ui/styles';
 import { Container, Paper, Typography } from '@material-ui/core';
 import { Box } from '@material-ui/system';
-import { CardMedia, CardHeader } from '@material-ui/core';
+import { CardMedia, CardHeader, Card } from '@material-ui/core';
+import Avatar from '@material-ui/core/Avatar';
+import { createTheme } from '@material-ui/system';
 
+const Theme = createTheme();
 const useStyles = makeStyles((theme) => ({
   root: {
     maxHeight: '100vh',
@@ -23,6 +26,14 @@ const useStyles = makeStyles((theme) => ({
   box: {
     borderRadius: '20%',
   },
+  avatars: {
+    display: 'flex',
+    margin: 3,
+  },
+  small: {
+    width: Theme.spacing(3),
+    height: Theme.spacing(3),
+  },
 }));
 
 const defaultProps = {
@@ -34,16 +45,24 @@ const DepartmentDetails = ({ match }) => {
   const classes = useStyles();
   let department = departments.find((dep) => dep._id === match.params.id);
   const projects = department.projects[0];
-  console.log(department);
+  let teams = department.teams[0];
+  let project = teams.projects[0].title;
+  let people = teams.people.map((p) => {
+    return (
+      <div className={classes.avatars}>
+        <Avatar className={classes.small}>{p}</Avatar>
+      </div>
+    );
+  });
 
   return (
     <Container maxWidth='lg' className={classes.root}>
       <Box
         display='flex'
-        {...defaultProps}
         className={classes.wrapper}
         alignItems='flex-start'
         flexWrap='wrap'
+        {...defaultProps}
       >
         <Box
           display='flex'
@@ -87,7 +106,7 @@ const DepartmentDetails = ({ match }) => {
         <Box>
           <Paper elevation={3} className={classes.paper}>
             <CardHeader title='Projects' />
-            <Box display='flex' alignItems='center' {...defaultProps}>
+            <Box display='flex' alignItems='center' >
               <Typography component='span' color='primary'>
                 Description:
               </Typography>
@@ -96,21 +115,21 @@ const DepartmentDetails = ({ match }) => {
               </Typography>
             </Box>
 
-            <Box display='flex' alignItems='center' {...defaultProps}>
+            <Box display='flex' alignItems='center' >
               <Typography component='span' color='primary'>
                 Status:
               </Typography>
               <Typography pl={9}>{projects.status}</Typography>
             </Box>
 
-            <Box display='flex' alignItems='center' {...defaultProps}>
+            <Box display='flex' alignItems='center' >
               <Typography component='span' color='primary'>
                 Percent Complete:
               </Typography>
               <Typography pl={2}>{projects.percentComplete}%</Typography>
             </Box>
 
-            <Box display='flex' alignItems='center' {...defaultProps}>
+            <Box display='flex' alignItems='center'>
               <Typography component='span' color='primary'>
                 Deadline:
               </Typography>
@@ -119,9 +138,58 @@ const DepartmentDetails = ({ match }) => {
           </Paper>
         </Box>
 
-        <Box pl={2} py={2} display='flex' justifyItems='center'>
+        <Box style={{width: '60%'}} position="relative" left={50}>
           <Paper elevation={3} className={classes.paper}>
             <CardHeader title='Teams' />
+            <Box display='flex' alignItems='center'>
+              <Typography component='span' color='primary'>
+                Title:
+              </Typography>
+              <Typography pl={10} component='h2' variant='h6'>
+                {teams.title}
+              </Typography>
+            </Box>
+
+            <Box display='flex' alignItems='center'>
+              <Typography component='span' color='primary'>
+                Description:
+              </Typography>
+              <Typography pl={4} color='warning.main'>
+                {teams.description}
+              </Typography>
+            </Box>
+
+            <Box display='flex' alignItems='center'>
+              <Typography component='span' color='primary'>
+                Department:
+              </Typography>
+              <Typography pl={4}>{department.title}</Typography>
+            </Box>
+
+            <Box display='flex' alignItems='center'>
+              <Typography component='span' color='primary'>
+                Projects:
+              </Typography>
+              <Typography pl={7}>{project}</Typography>
+            </Box>
+
+            <Box display='flex' alignItems='center' >
+              <Typography component='span' color='primary'>
+                People:
+              </Typography>
+              <Box display='flex' flexDirection='row' ml={8}>
+                {people}
+              </Box>
+            </Box>
+
+            <Box display='flex' alignItems='center'>
+              <Typography component='span' color='primary'>
+                Owner:
+              </Typography>
+              <Typography pl={9} color='warning.main'>
+                {teams.owner}
+              </Typography>
+            </Box>
           </Paper>
         </Box>
       </Box>
